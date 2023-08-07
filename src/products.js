a432b82f744f0e85498d32f01f70b86961c7b53a
Michael Echaluse <michaeletr@gmail.com>
2023-08-06 19:00:06 -0700

    Modified files

---
+++ src/products.js
@@ -0,0 +1,66 @@
+import {AmazonFS} from './assets/amazonfirestick.png';
+import {CanonCamera} from './assets/canonrebelt7.png';
+import {ElectricToothBrush} from './assets/electrictoothbrush.png';
+import {Ipad} from './assets/ipad9.png';
+import {Iphone} from './assets/iphone13.png';
+import {Macbook} from './assets/macbookpro.png';
+import {PS5} from './assets/ps5.png';
+import {SanDisk} from './assets/sandiskportssd.png';
+import {TV} from './assets/toshibatv.png';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Amazon Fire Stick",
+        price: 26.99,
+        productImage: AmazonFS
+    },
+    {
+        id: 2,
+        productName: "Canon Rebel T7 Camera",
+        price: 549.99,
+        productImage: CanonCamera   
+    },
+    {
+        id: 3,
+        productName: "Electric Toothbrush",
+        price: 349.99,
+        productImage: ElectricToothBrush   
+    },
+    {
+        id: 4,
+        productName: "Ipad 9th Generation",
+        price: 320.00,
+        productImage: Ipad   
+    },
+    {
+        id: 5,
+        productName: "Iphone 13",
+        price: 599.99,
+        productImage: Iphone   
+    },
+    {
+        id: 6,
+        productName: "Macbook Pro 13\"",
+        price: 1299,
+        productImage: Macbook   
+    },
+    {
+        id: 7,
+        productName: "PlayStation 5",
+        price: 499,
+        productImage: PS5  
+    },
+    {
+        id: 8,
+        productName: "SanDisk Portable SSD 128 GB",
+        price: 249.99,
+        productImage: SanDisk   
+    },
+    {
+        id: 9,
+        productName: "Toshiba TV 26\"",
+        price: 749.99,
+        productImage: TV   
+    }
+]
