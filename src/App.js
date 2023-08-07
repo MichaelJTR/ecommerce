456cf3531ccf0d0a1a183bbde959bc35318fefac
Michael Echaluse <michaeletr@gmail.com>
2023-08-06 19:43:44 -0700

    Added content to file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {Navbar} from './components/navbar';
+import {Shop} from './pages/shop';
+
 
 
 function App() {
