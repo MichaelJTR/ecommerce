b123c2bee6d5ec2b9864f8c814c3af73581b9ff7
Michael Echaluse <michaeletr@gmail.com>
2023-08-07 18:24:39 -0700

    Added new content to file

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,3 +1,27 @@
-import React from "react";
+import React, {useContext} from "react";
 import {PRODUCTS} from '../../products';
+import {ShopContext} from '../../context/shop-context';
 import './cart.css';
+import { useNavigate } from "react-router-dom";
+
+export function Cart() {
+    const {cartItems, getTotalCartAmount} = useContext(ShopContext);
+    const totalAmount = getTotalCartAmount;
+
+    const navigate = useNavigate();
+
+    return (
+        <div className="cart">
+            <div>
+                <h1>Your Cart Items</h1>
+            </div>
+            <div className="cartItems">
+                {PRODUCTS.map((product) => {
+                    if (cartItems[product.id] !== 0) {
+                        return <CartItem data={product}/>
+                    }
+                })}
+            </div>
+        </div>
+    )
+}
