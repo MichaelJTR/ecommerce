331cb110dd01d2aa49d34555b7f1416a43d08a36
Michael Echaluse <michaeletr@gmail.com>
2023-08-07 18:28:02 -0700

    Added new content to file

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,6 +1,7 @@
 import React, {useContext} from "react";
 import {PRODUCTS} from '../../products';
 import {ShopContext} from '../../context/shop-context';
+import {CartItem} from './cart-item';
 import './cart.css';
 import { useNavigate } from "react-router-dom";
 
@@ -22,6 +23,13 @@ export function Cart() {
                     }
                 })}
             </div>
+            {totalAmount > 0 ?
+            (<div className="checkout">
+                <p> Subtotal: ${totalAmount}</p>
+                <button onClick={() => navigate("/")}> Continue Shopping</button>
+                <button> Checkout</button>
+            </div>)
+            : (<h1> Your Cart is Empty</h1>)}
         </div>
     )
 }
